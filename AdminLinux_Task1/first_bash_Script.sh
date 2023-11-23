3766acff54ef9d8eeb3e73e4f6e1888802706c59
nourmoharram98 <nourmoharram998@gmail.com>
2023-11-23 06:49:35 +0200

    edit bash file

---
+++ AdminLinux_Task1/first_bash_Script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#add commands below
+mkdir myDirectory
+cd myDirectory
+mkdir secondDirectory
+cd secondDirectory
+touch myNotePaper
+cp myNotePaper ..
+cd ../
+mv myNotePaper myOldNotePaper
