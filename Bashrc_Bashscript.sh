c7bc9da13dca600cdec6bbbc9f299f41eef5a1f3
nourmoharram98 <nourmoharram998@gmail.com>
2023-12-09 12:28:15 +0200

    Task3 section 1 submission

---
+++ Bashrc_Bashscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if .bashrc file exists (-f) flag used to search for file in home directory
+if [ -f "$HOME/.bashrc" ]; then
+    # Append new environment variables to .bashrc and (>>) used to concate the new variables and (\)  is used to escape the effect of dollar sign
+    echo "export HELLO=\$HOSTNAME" >> "$HOME/.bashrc"
+    echo "LOCAL=\$(whoami)" >> "$HOME/.bashrc"
+    
+    # source used to run the bashrc file
+    source "$HOME/.bashrc"
+    
+    # Open another terminal 
+    gnome-terminal    
+else
+    echo ".bashrc file not found in home directory"
+fi
+
